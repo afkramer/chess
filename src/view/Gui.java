da0a3e17dfabc6b64c82050814cb2e420e8e51b9
afkramer <adri.kramer@gmail.com>
2023-03-23 15:36:18 +0100

    Set up ANSCI escape codes for chess pieces

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -1,5 +1,20 @@
 package view;
 
 public class Gui {
+	private static final String WHITE_KING = "\u2654";
+	private static final String WHITE_QUEEN = "\u2655";
+	private static final String WHITE_ROOK = "\u2656";
+	private static final String WHITE_BISHOP = "\u2657";
+	private static final String WHITE_KNIGHT = "\u2658";
+	private static final String WHITE_PAWN = "\u2659";
+	private static final String BLACK_KING = "\u265A";
+	private static final String BLACK_QUEEN = "\u265B";
+	private static final String BLACK_ROOK = "\u265C";
+	private static final String BLACK_BISHOP = "\u265D";
+	private static final String BLACK_KNIGHT = "\u265E";
+	private static final String BLACK_PAWN = "\u265F";
+	
+	private Gui() {}
+	
 
 }
